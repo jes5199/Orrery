f4cdb63b67b13d6b0a1f66a3518217f9310c70c2
Jesse Wolfe <jes5199@gmail.com>
2014-03-06 17:15:24 -0800

    Add an example 3D drawing to the window

---
+++ Orrery/POOpenGLView.h
@@ -0,0 +1,14 @@
+//
+//  POOpenGLView.h
+//  Orrery
+//
+//  Created by Jesse Wolfe on 3/6/14.
+//  Copyright (c) 2014 Jesse Wolfe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#include <OpenGL/gl.h>
+
+@interface POOpenGLView : NSOpenGLView
+
+@end
---
+++ Orrery/POOpenGLView.m
@@ -0,0 +1,43 @@
+//
+//  POOpenGLView.m
+//  Orrery
+//
+//  Created by Jesse Wolfe on 3/6/14.
+//  Copyright (c) 2014 Jesse Wolfe. All rights reserved.
+//
+
+#import "POOpenGLView.h"
+
+@implementation POOpenGLView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+static void drawAnObject ()
+{
+    glColor3f(1.0f, 0.85f, 0.35f);
+    glBegin(GL_TRIANGLES);
+    {
+        glVertex3f(  0.0,  0.6, 0.0);
+        glVertex3f( -0.2, -0.3, 0.0);
+        glVertex3f(  0.2, -0.3 ,0.0);
+    }
+    glEnd();
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    glClearColor(0, 0, 0, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawAnObject();
+    glFlush();
+}
+
+@end
